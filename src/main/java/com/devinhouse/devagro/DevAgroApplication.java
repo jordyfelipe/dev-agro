9e2aa05bc4573cdfe59471ab0307a7b45946b06c
Jordy Felipe da Silva <jordyf.silva@hotmail.com>
2022-03-12 13:22:43 -0300

    Project created

---
+++ src/main/java/com/devinhouse/devagro/DevAgroApplication.java
@@ -0,0 +1,13 @@
+package com.devinhouse.devagro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAgroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAgroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devinhouse/devagro/DevAgroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.devagro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAgroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
